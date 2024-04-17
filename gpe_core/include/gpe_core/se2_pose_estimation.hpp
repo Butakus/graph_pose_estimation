42aac63fbdd3d4ad8d69b23662036bb7cbaf5dd6
Fran <butakus@gmail.com>
2024-04-17 17:17:02 +0200

    Update license

--- gpe_core/include/gpe_core/se2_pose_estimation.hpp
+++ gpe_core/include/gpe_core/se2_pose_estimation.hpp
@@ -1,14 +1,25 @@
+// Copyright 2024 Francisco Miguel Moreno
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//  Author: Francisco Miguel Moreno
+
 #ifndef GPE_CORE__SE2_POSE_ESTIMATION_HPP_
 #define GPE_CORE__SE2_POSE_ESTIMATION_HPP_
 
-#include "gpe_core/visibility_control.h"
-
 #include <Eigen/StdVector>
 #include <Eigen/Dense>
-#include <iostream>
-#include <stdint.h>
-#include <vector>
-#include <cmath>
 
 #include <g2o/core/sparse_optimizer.h>
 #include <g2o/core/block_solver.h>
@@ -20,6 +31,10 @@
 #include <g2o/types/slam2d/vertex_point_xy.h>
 #include <g2o/types/slam2d/vertex_se2.h>
 #include <g2o/types/slam2d/edge_se2_pointxy.h>
+
+#include <iostream>
+#include <vector>
+#include <cmath>
 
 namespace gpe_core
 {
